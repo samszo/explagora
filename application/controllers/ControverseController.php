e74865ab858ea9d0cca22c10e3aa2133788eaca5
Samuel Szoniecky <samszon@gmail.com>
2017-05-10 07:21:09 +0200

    ajout formulaire

--- application/controllers/ControverseController.php
+++ application/controllers/ControverseController.php
@@ -15,6 +15,24 @@ class ControverseController extends Zend_Controller_Action
 	    	$this->view->uti = "{'uti_id':1,'role':'admin'}";
     	 
     }    
+
+    public function creerAction()
+    {	
+    		switch ($this->_getParam('table')) {
+    			case "Question":
+    				$bdQ = new Model_DbTable_Question();
+    				$this->view->id = $bdQ->ajouter(array("valeur"=>$this->_getParam('valeur')));    				
+    			break;    			
+    			case "Indice":
+    				//$bdQ = new Model_DbTable_Indice();
+    			
+    				break;
+    				 default:
+    				;
+    			break;
+    		}
+    	
+    }
     
 }
 
---
+++ application/models/DbTable/Question.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Classe ORM qui représente la table 'Question'.
+ *
+ * @author Samuel Szoniecky
+ * @category   Zend
+ * @package Zend\DbTable\Omk
+ * @license https://creativecommons.org/licenses/by-sa/2.0/fr/ CC BY-SA 2.0 FR
+ * @version  $Id:$
+ */
+class Model_DbTable_Question extends Zend_Db_Table_Abstract
+{
+    
+    /*
+     * Nom de la table.
+     */
+    protected $_name = 'Question';
+    
+    /*
+     * Clef primaire de la table.
+     */
+    protected $_primary = 'idQuestion';
+
+    protected $_dependentTables = array();
+    
+
+        
+    /**
+     * Vérifie si une entrée Question existe.
+     *
+     * @param array $data
+     *
+     * @return integer
+     */
+    public function existe($data)
+    {
+		$select = $this->select();
+		$select->from($this, array('id'));
+		foreach($data as $k=>$v){
+			$select->where($k.' = ?', $v);
+		}
+	    $rows = $this->fetchAll($select);        
+	    if($rows->count()>0)$id=$rows[0]->id; else $id=false;
+        return $id;
+    } 
+        
+    /**
+     * Ajoute une entrée Question.
+     *
+     * @param array $data
+     * @param boolean $existe
+     *  
+     * @return integer
+     */
+    public function ajouter($data, $existe=true)
+    {    	
+	    	$id=false;
+	    	if($existe)$id = $this->existe($data);
+	    	if(!$id){
+	    		if(!isset($data["created"])) $data["created"] = new Zend_Db_Expr('NOW()');
+	    		$id = $this->insert($data);
+	    	}
+	    	return $id;
+    } 
+           
+    /**
+     * Recherche une entrée Question avec la clef primaire spécifiée
+     * et modifie cette entrée avec les nouvelles données.
+     *
+     * @param integer $id
+     * @param array $data
+     *
+     * @return void
+     */
+    public function edit($id, $data)
+    {        
+   	
+    		$this->update($data, 'Question.idQuestion = ' . $id);
+    }
+    
+    /**
+     * Recherche une entrée Question avec la clef primaire spécifiée
+     * et supprime cette entrée.
+     *
+     * @param integer $id
+     *
+     * @return void
+     */
+    public function remove($id)
+    {    	
+	    	$this->delete('Question.idQuestion = ' . $id);
+    }
+    
+}
