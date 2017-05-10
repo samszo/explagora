719b688b9f72b568ec1599e88e28a99b0bcf9144
ikramghazali <ghazali.ikram@gmail.com>
2017-05-10 11:03:14 +0200

    Add files via upload

---
+++ application/models/DbTable/Indice.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Classe ORM qui représente la table 'Indice'.
+ *
+ * @author Samuel Szoniecky
+ * @category   Zend
+ * @package Zend\DbTable\Omk
+ * @license https://creativecommons.org/licenses/by-sa/2.0/fr/ CC BY-SA 2.0 FR
+ * @version  $Id:$
+ */
+class Model_DbTable_Indice extends Zend_Db_Table_Abstract
+{
+    
+    /*
+     * Nom de la table.
+     */
+    protected $_name = 'Indice';
+    
+    /*
+     * Clef primaire de la table.
+     */
+    protected $_primary = 'idIndice';
+    protected $_dependentTables = array();
+    
+        
+    /**
+     * Vérifie si une entrée Indice existe.
+     *
+     * @param array $data
+     *
+     * @return integer
+     */
+    public function existe($data)
+    {
+		$select = $this->select();
+		$select->from($this, array('id'));
+		foreach($data as $k=>$v){
+			$select->where($k.' = ?', $v);
+		}
+	    $rows = $this->fetchAll($select);        
+	    if($rows->count()>0)$id=$rows[0]->id; else $id=false;
+        return $id;
+    } 
+        
+    /**
+     * Ajoute une entrée Indice.
+     *
+     * @param array $data
+     * @param boolean $existe
+     *  
+     * @return integer
+     */
+    public function ajouter($data, $existe=true)
+    {    	
+	    	$id=false;
+	    	if($existe)$id = $this->existe($data);
+	    	if(!$id){
+	    		if(!isset($data["created"])) $data["created"] = new Zend_Db_Expr('NOW()');
+	    		$id = $this->insert($data);
+	    	}
+	    	return $id;
+    } 
+           
+    /**
+     * Recherche une entrée Indice avec la clef primaire spécifiée
+     * et modifie cette entrée avec les nouvelles données.
+     *
+     * @param integer $id
+     * @param array $data
+     *
+     * @return void
+     */
+    public function edit($id, $data)
+    {        
+   	
+    		$this->update($data, 'Indice.idIndice = ' . $id);
+    }
+    
+    /**
+     * Recherche une entrée Indice avec la clef primaire spécifiée
+     * et supprime cette entrée.
+     *
+     * @param integer $id
+     *
+     * @return void
+     */
+    public function remove($id)
+    {    	
+	    	$this->delete('Indice.idIndice = ' . $id);
+    }
+    
+}
