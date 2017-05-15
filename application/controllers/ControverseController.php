11b211f0b2e4a09fe8612e87937af266bf58c84c
Samuel Szoniecky <samszon@gmail.com>
2017-05-15 06:48:28 +0200

    controverses

--- application/controllers/ControverseController.php
+++ application/controllers/ControverseController.php
@@ -12,24 +12,28 @@ class ControverseController extends Zend_Controller_Action
     
     public function gererAction()
     {
-	    	$this->view->uti = "{'uti_id':1,'role':'admin'}";
+	    	$this->view->uti = "{'idUti':1,'role':'admin','nom':'explagora'}";
     	 
     }    
 
     public function creerAction()
     {	
     		switch ($this->_getParam('table')) {
+    			case "Controverse":
+    				echo "ICI";
+    				$data = $this->_getParam('data');
+    				$bdQ = new Model_DbTable_Question();
+    				$this->view->id = $bdQ->ajouter(array("valeur"=>$data["question"]));
+    				break;
     			case "Question":
     				$bdQ = new Model_DbTable_Question();
     				$this->view->id = $bdQ->ajouter(array("valeur"=>$this->_getParam('valeur')));    				
     			break;    			
     			case "Indice":
-    				//$bdQ = new Model_DbTable_Indice();
-    			
+    				//$bdQ = new Model_DbTable_Indice();    			
     				break;
-    				 default:
-    				;
-    			break;
+    			default:
+    				break;
     		}
     	
     }
